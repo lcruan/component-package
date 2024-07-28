6f9c1ac78fd33d2a45365bd932d039a87bf10ce9
lcruan <1416802331@qq.com>
2024-07-28 12:25:39 +0800

    收缩菜单组件抽离

--- m-components/src/components/container/src/index.vue
+++ m-components/src/components/container/src/index.vue
@@ -1,34 +1,11 @@
 <template>
   <el-container>
     <el-aside width="auto">
-      <el-menu
-        :collapse="isCollapse"
-        default-active="2"
-        class="el-menu-vertical-demo"
-      >
-        <el-menu-item index="1">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航2</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航3</span>
-        </el-menu-item>
-      </el-menu>
+      <nav-side :collapse="isCollapse"></nav-side>
     </el-aside>
     <el-container>
       <el-header>
-        <span @click="isCollapse = !isCollapse">
-          <el-icon-expand
-            v-if="isCollapse"
-            style="margin-right: 10px"
-          ></el-icon-expand>
-          <el-icon-fold v-else></el-icon-fold
-        ></span>
+        <nav-header v-model:collapse="isCollapse"></nav-header>
       </el-header>
       <el-main><router-view></router-view></el-main>
     </el-container>
@@ -36,10 +13,14 @@
 </template>
 <script setup>
 import { ref } from "vue";
+import NavHeader from "./navHeader/index.vue";
+import NavSide from "./navSide/index.vue";
+
 let isCollapse = ref(false);
 </script>
 <style scoped lang="scss">
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+.el-header {
+    padding: 0;
+    border-bottom: 1px solid #eee;
 }
 </style>
---
+++ m-components/src/components/container/src/navHeader/index.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="header">
+    <span @click="toggle">
+    <el-icon-expand v-if="collapse" style="margin-right: 10px"></el-icon-expand>
+    <el-icon-fold v-else></el-icon-fold
+  ></span>
+ </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+    collapse: {
+        type: Boolean
+    }
+})
+
+let emits = defineEmits(['update:collapse'])
+let toggle = () => {
+    // 需要父组件的数据
+    emits('update:collapse', !props.collapse)
+}
+</script>
+<style scoped lang="scss">
+.header {
+    height: 60px;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+}
+</style>
